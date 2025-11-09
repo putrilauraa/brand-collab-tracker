bcdfb290b9f093482a03b755c775848842ad188e
putrilauraa <rahmpautrilaura@gmail.com>
2025-11-09 23:49:24 +0700

    fix(project): resolve nested preload

--- repositories/project_repository.go
+++ repositories/project_repository.go
@@ -41,8 +41,7 @@ func GetProjects() ([]models.Project, error) {
 
 func GetProjectByID(id uint) (*models.Project, error) {
 	var project models.Project
-	// err := config.DB.Preload("Brand.Category").Preload("Tasks").Preload("Attachments").First(&project, id).Error
-	err := config.DB.First(&project, id).Error
+	err := config.DB.Preload("Brand.Category").Preload("Tasks").Preload("Attachments").First(&project, id).Error
 	return &project, err
 }
 
